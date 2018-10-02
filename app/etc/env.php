7d5b0c4aeacf88bff2a3c3ddc58b0f9eeed6ac5b
IMPELSYS\anil.gupta <anil.gupta@laerdal.com>
2018-10-02 20:48:52 +0530

    made some change in env

--- app/etc/env.php
+++ app/etc/env.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'backend' => [
-        'frontName' => 'testadmin-3'
+        'frontName' => 'testadmin-4'
     ],
     'crypt' => [
         'key' => 'b3162007563a5602a31395b9a5193669'
